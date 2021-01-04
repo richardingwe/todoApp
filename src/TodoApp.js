4672c8ed74460064cbec3986306e1b46073ec1ad
Richard Ingwe <richardingwe5@gmail.com>
2021-01-04 12:30:24 +0100

    added todo context

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -7,6 +7,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import Grid from "@material-ui/core/Grid";
+import { TodosProvider } from "./context/todos.context";
 
 export default function TodoApp() {
     const initialTodos = [];
@@ -29,13 +30,15 @@ export default function TodoApp() {
             </AppBar>
             <Grid container justify="center" style={{ marginTop: "1rem" }}>
                 <Grid item xs={11} md={8} lg={4}>
-                    <TodoForm addTodo={addTodo} />
-                    <TodoList
-                        todos={todos}
-                        removeTodo={removeTodo}
-                        toggleTodo={toggleTodo}
-                        editTodo={editTodo}
-                    />
+                    <TodosProvider>
+                        <TodoForm addTodo={addTodo} />
+                        <TodoList
+                            todos={todos}
+                            removeTodo={removeTodo}
+                            toggleTodo={toggleTodo}
+                            editTodo={editTodo}
+                        />
+                    </TodosProvider>
                 </Grid>
             </Grid>
         </Paper>
